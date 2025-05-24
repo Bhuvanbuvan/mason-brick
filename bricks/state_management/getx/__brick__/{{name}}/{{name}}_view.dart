57cfb47736a582bd5c6a0b33dcc245f74e9c400e
BHUVANESHWARAN K <106652913+Bhuvanbuvan@users.noreply.github.com>
2025-05-24 15:43:36 +0530

    for init commit

---
+++ bricks/state_management/getx/__brick__/{{name}}/{{name}}_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class {{name}}View extends StatelessWidget {
+  const {{name}}View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

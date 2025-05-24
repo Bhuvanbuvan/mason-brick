4826f29aed9e882c4c80472853ffab2a90f5b183
BHUVANESHWARAN K <106652913+Bhuvanbuvan@users.noreply.github.com>
2025-05-24 17:04:59 +0530

    confitgure setup

---
+++ bricks/app_utils/config/__brick__/{{name}}/image.dart
@@ -0,0 +1,7 @@
+class ImageConfig {
+  ///App Logo
+  static const String appLogo = "assets/images/appLogo.png";
+  static const String appLogoDark = "assets/images/appLogoDark.png";
+  static const String shoppers = "assets/images/shoppers.png";
+
+}
---
+++ bricks/app_utils/config/__brick__/{{name}}/string_config.dart
@@ -0,0 +1,6 @@
+class StringConfig {
+  static const String flag = "flag";
+
+  ///App Name
+  static const String appName = "Shoppers E-commerce Flutter UI Kit";
+}
